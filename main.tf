57266a3769299137ad66ec8e0d9577ab70529c37
Hiroshi Muraoka <h.muraoka714@gmail.com>
2020-07-28 00:34:53 +0900

    Add function & scheduler & pubsub

--- main.tf
+++ main.tf
@@ -33,6 +33,44 @@ terraform {
   }
 }
 
+resource "google_pubsub_topic" "topic-from" {
+  name = "topic-from"
+}
+
+resource "google_pubsub_topic" "topic-to" {
+  name = "topic-to"
+}
+
+resource "google_cloud_scheduler_job" "job" {
+  name        = "minutely"
+  description = "minutely"
+  schedule    = "*/1 * * * *"
+
+  pubsub_target {
+    topic_name = google_pubsub_topic.topic-from.id
+    data       = base64encode("test")
+  }
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "filter-holidays"
+  description = "filter holidays"
+  runtime     = "go113"
+
+  available_memory_mb   = 128
+  entry_point           = "Filter"
+
+	event_trigger {
+    event_type = "providers/cloud.pubsub/eventTypes/topic.publish"
+    resource   = "${google_pubsub_topic.topic-from.name}"
+  }
+
+  enviroment_variables = {
+    GCP_PROJECT = var.PROJECT
+    TARGET_TOPIC = "${google_pubsub_topic.topic-to.name}"
+  }
+}
+
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
